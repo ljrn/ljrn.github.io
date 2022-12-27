6425f3ba361544f31893cae429d5389fbc8ea611
ljrn <louisjrnm@gmail.com>
2022-12-27 09:07:08 +0100

    Fin test email

--- lang.js
+++ lang.js
@@ -229,7 +229,7 @@ function typeWriter(container) {
   typeWriterExecute(txt, i, container, speed);
 }
 
-/*window.onload = function () {
+window.onload = function () {
   document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
     // generate a five digit number for the contact_number variable
@@ -254,4 +254,4 @@ function typeWriter(container) {
         }, 4000);
       });
   });
-}*/
+}
